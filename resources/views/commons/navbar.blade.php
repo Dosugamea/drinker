4d9e706e9dcc61afc23d3373cf39366b16a67c60
dosugamea <dsgamer777@gmail.com>
2021-10-18 08:00:28 +0000

    Add header and page templates

---
+++ resources/views/commons/navbar.blade.php
@@ -0,0 +1,47 @@
+<header class="mb-4">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
+        <a class="navbar-brand" href="/">Drinker</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar" aria-expanded="true">
+        <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse show" id="nav-bar" style="">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item mx-4">
+                    <form class="d-flex">
+                        <input class="form-control" type="text" placeholder="Search">
+                        <button class="btn btn-secondary" type="submit">Search</button>
+                    </form>
+                </li>
+                <li class="nav-item dropdown mx-2">
+                    <a class="nav-link dropdown-toggle" id="btnCategory" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">カテゴリ</a>
+                    <div class="dropdown-menu" aria-labelledby="btnCategory">
+                        <a class="dropdown-item" href="#">水飲料</a>
+                        <a class="dropdown-item" href="#">お茶飲料</a>
+                        <a class="dropdown-item" href="#">珈琲飲料</a>
+                        <a class="dropdown-item" href="#">紅茶飲料</a>
+                        <a class="dropdown-item" href="#">果実飲料</a>
+                        <a class="dropdown-item" href="#">炭酸飲料</a>
+                        <a class="dropdown-item" href="#">乳酸飲料</a>
+                        <a class="dropdown-item" href="#">スポーツ飲料</a>
+                        <a class="dropdown-item" href="#">野菜飲料</a>
+                        <a class="dropdown-item" href="#">お酢飲料</a>
+                        <a class="dropdown-item" href="#">栄養ドリンク</a>
+                        <a class="dropdown-item" href="#">エナジードリンク</a>
+                        <a class="dropdown-item" href="#">ネタ枠</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown mx-2">
+                    <a class="nav-link dropdown-toggle" id="btnRanking" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">ランキング</a>
+                    <div class="dropdown-menu" aria-labelledby="btnRanking">
+                        <a class="dropdown-item" href="#">総合数</a>
+                        <a class="dropdown-item" href="#">試飲記録数</a>
+                        <a class="dropdown-item" href="#">購買数</a>
+                    </div>
+                </li>
+            </ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="#" class="btn btn-secondary">Twitterでログイン</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Drinker</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        {{-- Bootswatch Lumenテーマ --}}
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.6.0/dist/lumen/bootstrap.min.css">
+    </head>
+
+    <body>
+
+        {{-- ナビゲーションバー --}}
+        @include('commons.navbar')
+
+        <div class="container">
+            {{-- エラーメッセージ --}}
+            @include('commons.error_messages')
+
+            @yield('content')
+        </div>
+        {{-- BootStrap4の最新にアップデート --}}
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
