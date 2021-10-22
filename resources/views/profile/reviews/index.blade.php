d808741426ac9a18e90d825b5b772758face720b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 18:37:03 +0000

    🚧 Use search layout on search pages

--- resources/views/profile/reviews/index.blade.php
+++ resources/views/profile/reviews/index.blade.php
@@ -1,12 +1,7 @@
 @extends('layouts.search')
 
 @section('content')
-    <div class="center jumbotron rounded-0 custom-tron">
-        <div class="text-center text-white mt-5">
-            <h1>Drinker</h1>
-            <h1>毎日のゴクゴクをシェアしよう</h1>
-        </div>
-    </div>
-    <div class="container-lg mt-4">
-    </div>
+@for ($i = 0; $i < 5; $i++)
+@include('components.card_review')
+@endfor
 @endsection
