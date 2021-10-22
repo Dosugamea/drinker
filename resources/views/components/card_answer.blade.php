baed262b79cc612f2cfc14be4dce1e5bd86099bf
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 17:15:10 +0000

    🚧 Copied some components from temp

---
+++ resources/views/components/card_answer.blade.php
@@ -0,0 +1,21 @@
+<div class="col-md-10">
+    <div class="card rounded-0 bg-white shadow-sm text-center my-2">
+        <div class="row justify-content-center mt-2">
+            <div class="col-md-8">
+                <p class="mx-2 mt-1">これだからしろうとはダメだ！　もっとよく見ろ！</p>
+                <h4 class="mx-2 my-1">
+                    <i class="fas fa-user"></i>
+                    ユーザー名 <small>2021/12/04 12:04</small>
+                </h4>
+            </div>
+            <div class="col-md-4 my-2">
+                <div class="btn-group" role="group" aria-label="Basic example">
+                    <button class="btn btn-secondary" disabled>評価スコア</button>
+                    <button class="btn btn-primary">+</button>
+                    <button class="btn btn-secondary" disabled>1</button>
+                    <button class="btn btn-primary">-</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/product_title.blade.php
@@ -0,0 +1,29 @@
+<div class="row">
+    <div class="col-md-6 text-center">
+        <div class="h-100">
+            <h3 class="mt-3">Q&A詳細</h3>
+        </div>
+    </div>
+    <div class="col-md-6 mt-2 mt-md-0 text-center">
+        <div class="card rounded-0 bg-white shadow-sm">
+            <div class="row">
+                <div class="col-4 my-auto">
+                    <img src="http://placehold.jp/320x240.png" class="img-fluid rounded-0">
+                </div>
+                <div class="col-8">
+                    <h4 class="mt-2">ドリンク名</h4>
+                    <h6>メーカー名</h6>
+                    <p>
+                        <i class="fas fa-star text-warning"></i>
+                        <i class="fas fa-star text-warning"></i>
+                        <i class="fas fa-star text-warning"></i>
+                        <i class="fas fa-star text-warning"></i>
+                        <i class="fas fa-star-half-alt text-warning"></i>
+                        4.5 |
+                        <small>評価者数: 1名</small>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
