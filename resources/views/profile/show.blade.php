297fc2488ebf965b97815a5cc0eeff244468a691
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 19:03:13 +0000

    🚧 Create index-logined page blade

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,12 +1,62 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron rounded-0 custom-tron">
-        <div class="text-center text-white mt-5">
-            <h1>Drinker</h1>
-            <h1>毎日のゴクゴクをシェアしよう</h1>
+<div class="row">
+    <div class="col-md-8">
+        <div class="container mt-4">
+            <div class="row">
+                <div class="col-md mt-2">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <p class="card-text">
+                                <p>ユーザー名</p>
+                                <p>レビュアー Lv1</p>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md mt-2">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <p class="card-text">
+                                <p>試飲記録: 0記事</p>
+                                <p>購買記録: 0記事</p>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <button type="button" class="btn btn-primary btn-lg btn-block my-4">試飲記録をつける</button>
+        <button type="button" class="btn btn-primary btn-lg btn-block my-4">購買記録をつける</button>
+        <button type="button" class="btn btn-secondary btn-lg btn-block my-4">試飲記録一覧</button>
+        <button type="button" class="btn btn-secondary btn-lg btn-block my-4">購買記録一覧</button>
+        <button type="button" class="btn btn-secondary btn-lg btn-block my-4">質問に回答する</button>
+        <button type="button" class="btn btn-secondary btn-lg btn-block my-4">ユーザー設定</button>
+    </div>
+    <div class="col-md-4 mt-3">
+        <h3 class="mt-4">人気ドリンク</h3>
+        @for ($i = 0; $i < 3; $i++)
+            <div class="row mx-2">
+                @include('components.card_beverage')
+            </div>
+        @endfor
+        <h3 class="mt-4">イチオシドリンク</h3>
+        @for ($i = 0; $i < 3; $i++)
+            <div class="row mx-2">
+                @include('components.card_beverage')
+            </div>
+        @endfor
+        <h3 class="mt-4">統計</h3>
+        <div class="row card mx-2">
+            <div class="card-body">
+                <p class="card-text">
+                    <p>ユーザーに飲まれた本数: XX本(ダム 0基分)</p>
+                    <p>ユーザーの試飲記録数: YY記事(同人誌0冊分)</p>
+                    <p>登録ユーザー数: 1人</p>
+                </p>
+            </div>
         </div>
     </div>
-    <div class="container-lg mt-4">
-    </div>
+</div>
 @endsection
