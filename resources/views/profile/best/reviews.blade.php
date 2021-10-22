35b035fb751bbd1d64eddd84b2a0a8b527d13e24
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 18:56:59 +0000

    🚧 use list/list-with-beverage layout on pages

--- resources/views/profile/best/reviews.blade.php
+++ resources/views/profile/best/reviews.blade.php
@@ -1,12 +1,1 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="center jumbotron rounded-0 custom-tron">
-        <div class="text-center text-white mt-5">
-            <h1>Drinker</h1>
-            <h1>毎日のゴクゴクをシェアしよう</h1>
-        </div>
-    </div>
-    <div class="container-lg mt-4">
-    </div>
-@endsection
+@extends('layouts.list')
