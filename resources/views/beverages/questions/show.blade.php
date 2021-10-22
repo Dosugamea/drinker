8a29dd1c9752bb5b0ead0ede8f0c5e8862154663
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 19:21:01 +0000

    🚧 Add beverage question show blade

--- resources/views/beverages/questions/show.blade.php
+++ resources/views/beverages/questions/show.blade.php
@@ -1,12 +1,38 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="center jumbotron rounded-0 custom-tron">
-        <div class="text-center text-white mt-5">
-            <h1>Drinker</h1>
-            <h1>毎日のゴクゴクをシェアしよう</h1>
+@include('components.title_with_beverage')
+
+<div class="card rounded-0 bg-white shadow-sm text-center mt-4">
+    <div class="row justify-content-center mt-2">
+        <div class="col-md-7">
+            <p class="mx-2 mt-1">以前発売していた フルーツティーと同じじゃないですか!</p>
+            <h4 class="mx-2">
+                <i class="fas fa-user"></i>
+                ユーザー名 <small>2021/12/04 12:04</small>
+            </h4>
         </div>
     </div>
-    <div class="container-lg mt-4">
+    <div class="row justify-content-center">
+        @for ($i = 0; $i < 6; $i++)
+        <div class="col-md-10">
+            @include('components.card_answer')
+        </div>
+        @endfor
     </div>
+</div>
+
+<div class="row justify-content-center text-center mt-4">
+    <div class="col-md-3 mx-1 my-1">
+        <a class="btn btn-primary w-75">回答を投稿する</a>
+    </div>
+</div>
+<div class="row justify-content-around text-center mt-4">
+    <div class="col-md-3 mx-1 my-1">
+        <a class="btn btn-primary w-75">Twitterでシェア</a>
+    </div>
+    <div class="col-md-3 mx-1 my-1">
+        <a class="btn btn-primary w-75">Facebookでシェア</a>
+    </div>
+</div>
 @endsection
