957ec1e6fc08d98384d6df55d21c8bc81395a103
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-10-22 18:04:14 +0000

    🚧 Create views blade files template

---
+++ resources/views/beverages/reviews/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.list_with_beverage')
+
+@section('content')
+    <div class="center jumbotron rounded-0 custom-tron">
+        <div class="text-center text-white mt-5">
+            <h1>Drinker</h1>
+            <h1>毎日のゴクゴクをシェアしよう</h1>
+        </div>
+    </div>
+    <div class="container-lg mt-4">
+    </div>
+@endsection
