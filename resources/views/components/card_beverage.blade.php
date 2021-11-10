69c6329b6be8706d63d28e8402a33bebb351d7a7
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-10 12:53:59 +0000

    🐛 🎨 Fix card_beverage column size

--- resources/views/components/card_beverage.blade.php
+++ resources/views/components/card_beverage.blade.php
@@ -1,10 +1,10 @@
 <div class="card rounded-0 bg-white shadow-sm">
-    <div class="row">
+    <div class="is-centered">
         <a href="{{ route('beverages.beverage', ['beverage_id'=>'1']) }}">
-            <div class="col-4 my-auto">
-                <img src="{{ $beverage->images()->get()[0]->path }}" class="img-fluid rounded-0">
+            <div class="col-12">
+                <img src="{{ $beverage->images()->get()[0]->path }}" class="img-fluid d-block mx-auto rounded-0">
             </div>
-            <div class="col-8">
+            <div class="col-12 is-centered">
                 <h4 class="mt-2">{{ $beverage->title }}</h4>
                 <h6>メーカー名</h6>
                 <p>
