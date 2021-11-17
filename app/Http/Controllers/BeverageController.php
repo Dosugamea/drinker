8b6c367c8237359ab6a9fb2abc9892ceda6b4cc3
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-17 13:02:10 +0000

    🚧 タグ一覧の表示と追加の実装

--- app/Http/Controllers/BeverageController.php
+++ app/Http/Controllers/BeverageController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Beverage;
+use App\BeverageTag;
+use App\Tag;
 
 class BeverageController extends Controller
 {
@@ -14,8 +17,15 @@ class BeverageController extends Controller
      */
     public function show($id)
     {
-        $beverage = Beverage::find($id);
-        return view('beverages.show', compact('beverage'));
+        $beverage = Beverage::findOrFail($id);
+        // get()した結果はObjectのため、map_array()で配列に変換できない
+        $db_tags = $beverage->tags()->get();
+        $ary_tags = [];
+        foreach($db_tags as $tag ) {
+            $ary_tags[] = $tag->name;
+        }
+        $tags = implode(', ', $ary_tags);
+        return view('beverages.show', compact('beverage', 'tags'));
     }
 
     /**
@@ -27,5 +37,41 @@ class BeverageController extends Controller
     {
         // TODO: Add beverage model
         return view('beverages.reviews.index');
+    }
+
+    /**
+     * Add specified tag to this beverage resource.
+     *
+     * @param  Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function addTag(Request $request, $id){
+        $beverage = Beverage::findOrFail($id);
+        $db_tags = $beverage->tags()->get();
+        $ary_tags = [];
+        foreach($db_tags as $tag ) {
+            $ary_tags[] = $tag->name;
+        }
+        $tagName = $request->input('name');
+        if (in_array($tagName, $ary_tags)) {
+            return response()->json(
+                ['status' => 'error', 'message' => 'Tag already exists.'],
+                400
+            );
+        }
+        $user_id = \Auth::id();
+        $tag = Tag::firstOrCreate(
+            ['name' => $tagName],
+            ['name_en' => $tagName, 'type' => 1, 'user_id' => $user_id]
+        );
+        $beverage_tag = BeverageTag::create([
+            'beverage_id' => $id,
+            'tag_id' => $tag->id,
+            'user_id' => $user_id
+        ]);
+        $beverage_tag->save();
+        return response()->json(
+            ['status' => 'success']
+        );
     }
 }
