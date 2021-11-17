a9a6f24da75c9eb88f61846683ae24425961d83e
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-17 13:29:19 +0000

    🔇 不要となったログを消去

--- app/Http/Controllers/ProductAskController.php
+++ app/Http/Controllers/ProductAskController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use RakutenRws_Client;
 use App\Beverage;
 
@@ -22,7 +21,6 @@ class ProductAskController extends Controller
         ];
         $this->validate($request, $rules);
         $jan_code = $request->input('jan_code');
-        Log::debug('ログサンプル', ['memo' => $jan_code]);
         // 既存ドリンクであればそれを返す
         $beverage = Beverage::where('jan_code', $jan_code)->first();
         if($beverage != NULL) {
@@ -58,7 +56,6 @@ class ProductAskController extends Controller
                 404
             );
         }
-        Log::debug('ログサンプル', ['memo' => $response]);
         // 商品情報から商品名だけを取り出す
         $productNames = array();
         foreach ($response as $product) {
@@ -76,11 +73,9 @@ class ProductAskController extends Controller
         }, $productNames);
         // 二次元配列になっているので一次元に戻す
         $keywords = array_reduce($keywords, 'array_merge', []);
-        Log::debug('ログサンプル', ['memo' => $keywords]);
         // 配列内の要素を数えて キー/個数 の連想配列作成
         $counts = array_count_values($keywords);
         // 2回以上出現するキーワードのみに絞り込み
-        Log::debug('ログサンプル', $counts);
         $counts = array_filter($counts, function($element) {
             return $element > 1;
         });
