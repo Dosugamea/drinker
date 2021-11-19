3f20e87996be00d4071b2a4fcf7d65a4feb0a343
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-19 13:00:03 +0000

    💡 ✨ 検索にタグでの絞り込みを追加

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Review;
 use App\Beverage;
+use App\Tag;
 
 class SearchController extends Controller
 {
@@ -16,18 +17,41 @@ class SearchController extends Controller
      */
     public function search(Request $request)
     {
-        $searchType = $request->input('type');
+        // 要求出力: reviewまたはbeverage
+        $searchType = $request->input('type', 'beverage');
+        // 検索キーワード: 任意の文字列
+        $query = $request->input('query', '');
+        // レビューを検索する場合
         if ($searchType == 'review') {
-            $reviews = Review::where('title', 'like', '%' . $request->input('query') . '%')
-            ->orWhere('body', 'like', '%' . $request->input('query') . '%')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+            $reviews = Review::orderBy('created_at', 'desc');
+            // キーワードで絞り込み
+            if ($query != '') {
+                $reviews = $reviews->where('title', 'like', '%' . $query . '%')
+                ->orWhere('body', 'like', '%' . $query . '%');
+            }
+            // ページネーション生成
+            $reviews = $reviews->paginate(10);
             return view('search', compact('reviews'));
+        // 飲み物を検索する場合
         } else {
-            $beverages = Beverage::where('title', 'like', '%' . $request->input('query') . '%')
-            ->orWhere('description', 'like', '%' . $request->input('query') . '%')
-            ->orderBy('created_at', 'desc')
-            ->paginate(10);
+            $beverages = Beverage::orderBy('created_at', 'desc');
+            // タグで絞り込み
+            $searchTags = [$request->input('tag'), $request->input('category')];
+            foreach ($searchTags as $searchTag) {
+                if ($searchTag != NULL) {
+                    $tag = Tag::where('name', $searchTag)->firstOrFail();
+                    $beverages = $beverages->whereHas('tags', function ($query) use ($tag) {
+                        $query->where('tag_id', $tag->id);
+                    });
+                }
+            }
+            // キーワードで絞り込み
+            if ($query != '') {
+                $beverages = $beverages->where('title', 'like', '%' . $query . '%')
+                ->orWhere('description', 'like', '%' . $query . '%');
+            }
+            // ページネーション生成
+            $beverages = $beverages->paginate(10);
             return view('search', compact('beverages'));
         }
     }
