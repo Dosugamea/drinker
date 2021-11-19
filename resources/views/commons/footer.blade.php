e8babfbd46a675649b68fd8f5dc84db067cd1d06
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-19 17:39:32 +0000

    🔥 間に合わないので静的ページ全般リンク消去

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,6 +1,9 @@
 <footer class="mt-3 text-center py-3">
+    {{--
     <a href="#">利用規約</a> |
     <a href="#">プライバシーポリシー</a> |
     <a href="#">ヘルプ</a> |
     <a href="#">お問い合わせ</a>
+    --}}
+    <p>Copyright © 2021 Drinker All Rights Reserved.</p>
 </footer>
