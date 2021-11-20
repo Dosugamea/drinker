32299c423d8b6f479401a64be919b3f8bf16643b
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-20 11:06:41 +0000

    ✨ 検索結果ヒット数がゼロの場合見つからなかったと表示

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -7,6 +7,15 @@
     @include('components.card_review')
 </div>
 @endforeach
+@if($reviews->count() == 0)
+<div class="col-md-5 my-2 mx-2">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">一致するコンテンツがありませんでした</h5>
+        </div>
+    </div>
+</div>
+@endif
 @endisset
 @isset($beverages)
 @foreach ($beverages as $beverage)
@@ -14,5 +23,14 @@
     @include('components.card_beverage')
 </div>
 @endforeach
+@if($beverages->count() == 0)
+<div class="col-md-5 my-2 mx-2">
+    <div class="card">
+        <div class="card-body">
+            <h5 class="card-title">一致するコンテンツがありませんでした</h5>
+        </div>
+    </div>
+</div>
+@endif
 @endisset
 @endsection
