82da19abe6a54a4f01721ce5aada857f004b33d6
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-11-21 09:21:47 +0000

    🚸 ランキング表記の追加

--- resources/views/rankings/shared.blade.php
+++ resources/views/rankings/shared.blade.php
@@ -2,6 +2,7 @@
 @section('cards')
 @foreach ($beverages as $beverage)
 <div class="col-md-5 my-2 mx-2">
+    <p class="text-center">第{{ $loop->iteration}}位<p>
     @include('components.card_beverage')
 </div>
 @endforeach
